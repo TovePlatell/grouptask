0e08f879beaafc700219f46c8750be9573a3e1ee
tove platell <tove.platell@medieinstitutet.se>
2021-09-30 09:34:08 +0200

    added manual not finish

---
+++ manual.php
@@ -0,0 +1,18 @@
+<?php /* Template Name: manual */ ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+
+    <div id="wrap">
+
+        <?php get_header(); ?>
+
+        <div class="hero">
+            <h1>Manual för web-shopen</h1>
+        </div>
