2085fc41e9bca39805f3fdc47723ab2aedcba3ca
tove platell <tove.platell@medieinstitutet.se>
2021-10-04 22:28:09 +0200

    some small styling

--- contact.php
+++ contact.php
@@ -4,13 +4,9 @@
 <html>
 
 <head>
-    <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8" />
-    <title>Labb 1</title>
-    <link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" />
-    <script src="js/jquery.js"></script>
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -19,23 +15,26 @@
 
         <?php get_header(); ?>
 
-    <div class = "hero"><h1>Kontakta oss</h1></div>
+        <div class="hero">
+            <h1>Kontakta oss</h1>
+        </div>
 
-    <?php
+        <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-    <?php
+        <?php the_content(); ?>
+        <?php
         endwhile;
     endif;
     ?>
 
 
-    <div class="location">
-    <h2>Här finns vi</h2>
+        <div class="location">
+            <h2 class="locationtitle">Här finns vi</h2>
 
-  <?php
+            <p class=" storelocation">
 
+                <?php
 
 $args = array(  
     'post_type' => 'stores',
@@ -47,20 +46,20 @@ $args = array(
 );
 $loop = new WP_Query( $args ); 
     
-while ( $loop->have_posts() ) : $loop->the_post(); ?>
-    
-    <a href="<?php the_permalink(); ?>"> <?php echo the_title()?> </a>
-   
-   
-<?php
+while ( $loop->have_posts() ) : $loop->the_post(); ?> <a href="<?php the_permalink(); ?>"> <?php echo the_title()?>
+                </a>
+
+
+                <?php
 endwhile;
 wp_reset_postdata();
 
 ?>
-   
-   
+            </p>
+        </div>
 
 
-        
+
+
 
         <?php get_footer(); ?>
